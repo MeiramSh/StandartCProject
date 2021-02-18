5730bbc288e7fa2569bba3dfeb560b77a51f5f68
Meiram <meiram.shunshalin@nu.edu.kz>
2021-02-18 17:58:34 +0600

    init

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <adder.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	int i = 0;
+	printf("Hello World\n");
+	int k = 3;
+
+	if (i == 0) {
+		k = 3;
+		int j = 4;
+	}
+	float a = 3, b = 8;
+	printf("%f\n", add(a, b));
+
+	printf("hey:%d", 9);
+}
